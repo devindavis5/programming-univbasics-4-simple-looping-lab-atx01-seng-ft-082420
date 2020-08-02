0129df9564b87edf227c2b10ebf69f4a40793ec8
Devin Davis <devinrdavis555@gmail.com>
2020-08-02 22:41:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,12 @@ def loop_message_n_times(string, n)
     counter += 1
   end 
 end
+
+def output_array(array)
+  counter = 0
+  
+  while counter <= array.length do
+  puts i
+  counter += 1
+end
+end
